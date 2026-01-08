14292880d5c81270e239e289d9bbc5f13cde8a31
webgnar <zgeogoulet@gmail.com>
2026-01-08 14:58:05 -0600

    stable b4 incremental

--- .godot/mono/temp/obj/Debug/fps.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/fps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c6e7ed37fb455abc61a2947a9a643afe3b7393e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+106cb0afeaa82e43e05492f0157895ce16fc234a")]
 [assembly: System.Reflection.AssemblyProductAttribute("fps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
