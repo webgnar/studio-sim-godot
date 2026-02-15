9579e065ecd4202c1b04e7a810994a63e1d69e98
webgnar <zgeogoulet@gmail.com>
2026-02-15 10:07:38 -0600

    added timer to mission hud

--- scripts/ui/MissionHUD.gd
+++ scripts/ui/MissionHUD.gd
@@ -5,7 +5,10 @@ class_name MissionHUD
 
 @onready var panel_container = $PanelContainer
 @onready var thumbnail_rect = $PanelContainer/MarginContainer/HBoxContainer/ThumbnailRect
-@onready var mission_name_label = $PanelContainer/MarginContainer/HBoxContainer/MissionNameLabel
+@onready var mission_name_label = $PanelContainer/MarginContainer/HBoxContainer/VBoxContainer/MissionNameLabel
+@onready var timer_label = $PanelContainer/MarginContainer/HBoxContainer/VBoxContainer/TimerLabel
+
+var mission_active := false
 
 func _ready():
 	# Initially hidden - will show when mission starts
@@ -38,8 +41,18 @@ func _on_mission_started(mission: PaintingMission):
 		thumbnail_rect.texture = null
 
 	# Show the HUD
+	mission_active = true
 	visible = true
+
+func _process(_delta):
+	if mission_active and MissionManager and MissionManager.mission_start_time > 0:
+		var elapsed_ms = Time.get_ticks_msec() - MissionManager.mission_start_time
+		var total_seconds = int(elapsed_ms / 1000.0)
+		var minutes = total_seconds / 60
+		var seconds = total_seconds % 60
+		timer_label.text = "%d:%02d" % [minutes, seconds]
 
 func _on_mission_ended(_mission: PaintingMission, _result = null):
 	"""Hide HUD when mission ends"""
+	mission_active = false
 	visible = false
