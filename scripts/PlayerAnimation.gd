4e565cc46999451afe9f8bedc22548ad44054979
webgnar <zgeogoulet@gmail.com>
2026-02-24 00:19:33 -0600

    crouch animation

--- scripts/PlayerAnimation.gd
+++ scripts/PlayerAnimation.gd
@@ -14,6 +14,8 @@ var _is_moving: bool = false
 var _is_sprinting: bool = false
 var _is_jumping: bool = false
 var _was_crouching: bool = false
+var _crouch_entered: bool = false  # true after "crouch" transition completes
+var _un_crouching: bool = false    # true while playing "crouch" backwards
 
 func _ready() -> void:
 	# Get reference to AnimationPlayer node
@@ -23,9 +25,15 @@ func _ready() -> void:
 		push_warning("AnimationPlayer not found! Please check scene structure.")
 		return
 
-	# Crouch should play once and hold the last frame as the idle pose
+	# Crouch plays once as a transition; crouchidle and crouchwalk loop
 	if animation_player.has_animation("crouch"):
 		animation_player.get_animation("crouch").loop_mode = Animation.LOOP_NONE
+	if animation_player.has_animation("crouchidle"):
+		animation_player.get_animation("crouchidle").loop_mode = Animation.LOOP_LINEAR
+	if animation_player.has_animation("crouchwalk"):
+		animation_player.get_animation("crouchwalk").loop_mode = Animation.LOOP_LINEAR
+
+	animation_player.animation_finished.connect(_on_animation_finished)
 
 # Called by PlayerController to update animation state
 func update_animation_state(velocity: Vector3, is_on_floor: bool, is_sprinting: bool, is_crouching: bool = false) -> void:
@@ -42,19 +50,31 @@ func update_animation_state(velocity: Vector3, is_on_floor: bool, is_sprinting: 
 			if not play_animation("jump", jump_speed):
 				play_animation("idle", idle_speed)
 			_is_jumping = true
+			_crouch_entered = false
+			_un_crouching = false
 	else:
 		_is_jumping = false
 
-		# Detect un-crouch: play crouch animation in reverse to stand back up
 		if _was_crouching and not is_crouching:
+			# Start un-crouch: play transition backwards
+			_un_crouching = true
+			_crouch_entered = false
 			if animation_player.has_animation("crouch"):
 				animation_player.play_backwards("crouch")
 				animation_player.speed_scale = crouch_speed
+		elif _un_crouching:
+			# Don't interrupt the backwards crouch transition
+			pass
 		elif is_crouching:
 			if _is_moving:
 				play_animation("crouchwalk", crouch_speed)
+			elif _crouch_entered:
+				play_animation("crouchidle", crouch_speed)
 			else:
-				play_animation("crouch", crouch_speed)
+				# Play enter-crouch transition if not already playing
+				if animation_player.current_animation != "crouch":
+					animation_player.play("crouch")
+					animation_player.speed_scale = crouch_speed
 		elif _is_moving:
 			if _is_sprinting:
 				play_animation("run", run_speed)
@@ -64,6 +84,13 @@ func update_animation_state(velocity: Vector3, is_on_floor: bool, is_sprinting: 
 			play_animation("idle", idle_speed)
 
 	_was_crouching = is_crouching
+
+func _on_animation_finished(anim_name: String) -> void:
+	if anim_name == "crouch":
+		if _un_crouching:
+			_un_crouching = false
+		else:
+			_crouch_entered = true
 
 func play_animation(animation_name: String, speed: float = 1.0) -> bool:
 	if animation_player != null and animation_player.has_animation(animation_name):
